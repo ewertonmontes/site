ac7bff8bfcbeee0e6b9c224efcaa0f82516771b1
ewertonmontes <120966006+ewertonmontes@users.noreply.github.com>
2023-01-14 12:40:03 -0300

    Initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,34 @@
+/* === Expandir menu hambúrguer no clique === */
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".menu-alternativo",
+    ".menu1",
+  );
+  mobileNavbar.init();
+  /* === Fim === */
